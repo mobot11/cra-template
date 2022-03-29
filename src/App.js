cadb055335a2c8f6e7122bb53ac197d9bc9772f3
Dylan Moberg <dylan.moberg@highspot.com>
2022-03-28 19:33:12 -0700

    create template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css, Global } from '@emotion/react/macro';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Transactions } from './Transactions';
+
+const ErrorFallback = ({ error }) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global
+        styles={css`
+          *,
+          ::before,
+          ::after {
+            box-sizing: border-box;
+          }
+          html {
+            height: 100%;
+          }
+          body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+              'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            min-height: 100%;
+          }
+
+          code {
+            font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+          }
+        `}
+      />
+      <div
+        css={css`
+          width: 100vw;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+        `}
+      >
+        <main>
+          <ErrorBoundary FallbackComponent={ErrorFallback}>hello world</ErrorBoundary>
+        </main>
+      </div>
+    </>
   );
 }
 
---
+++ src/css/shared.js
@@ -0,0 +1,6 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react/macro';
+
+export const focusIndicator = css`
+  box-shadow: 0 0 0 2px #7fb6e3;
+`;
